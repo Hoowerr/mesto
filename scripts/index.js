eda673495534e89a78f9423595c4db2c37e25bec
Hoowerr <109082395+Hoowerr@users.noreply.github.com>
2022-12-03 03:09:24 +0300

    первый коммит

---
+++ scripts/index.js
@@ -0,0 +1,35 @@
+const popup = document.querySelector(".popup");
+const popupOpenButton = document.querySelector(".profile__edit-button");
+const popupCloseButton = document.querySelector(".popup__closed");
+const profileTitle = document.querySelector(".profile__title");
+const profileSubtitle = document.querySelector(".profile__subtitle");
+const popupNameInput = document.querySelector(".popup__input-name");
+const popupJobInput = document.querySelector(".popup__input-job");
+const popupForm = document.querySelector(".popup__form");
+const likeButtons = document.querySelectorAll(".element__like-button");
+
+function openPopup() {
+  popup.classList.add("popup__opened");
+  popupNameInput.value = profileTitle.textContent;
+  popupJobInput.value = profileSubtitle.textContent;
+}
+
+function closePopup() {
+  popup.classList.remove("popup__opened");
+}
+
+function save(event) {
+  event.preventDefault();
+  profileTitle.textContent = popupNameInput.value;
+  profileSubtitle.textContent = popupJobInput.value;
+  closePopup();
+}
+
+function like(event) {
+  event.target.classList.toggle("element__like-button_active");
+}
+
+popupForm.addEventListener("submit", save);
+popupOpenButton.addEventListener("click", openPopup);
+popupCloseButton.addEventListener("click", closePopup);
+likeButtons.forEach((button) => button.addEventListener("click", like));
