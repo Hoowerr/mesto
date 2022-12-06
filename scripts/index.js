b417bbb2c72861bed6161b6bfa5d76f3839eb5c1
Hoowerr <109082395+Hoowerr@users.noreply.github.com>
2022-12-07 00:23:54 +0300

    fixed

--- scripts/index.js
+++ scripts/index.js
@@ -3,19 +3,19 @@ const popupOpenButton = document.querySelector(".profile__edit-button");
 const popupCloseButton = document.querySelector(".popup__closed");
 const profileTitle = document.querySelector(".profile__title");
 const profileSubtitle = document.querySelector(".profile__subtitle");
-const popupNameInput = document.querySelector(".popup__input-name");
-const popupJobInput = document.querySelector(".popup__input-job");
+const popupNameInput = document.getElementById("name");
+const popupJobInput = document.getElementById("job");
 const popupForm = document.querySelector(".popup__form");
-const likeButtons = document.querySelectorAll(".element__like-button");
+// const likeButtons = document.querySelectorAll(".element__like-button");
 
 function openPopup() {
-  popup.classList.add("popup__opened");
+  popup.classList.add("popup_opened");
   popupNameInput.value = profileTitle.textContent;
   popupJobInput.value = profileSubtitle.textContent;
 }
 
 function closePopup() {
-  popup.classList.remove("popup__opened");
+  popup.classList.remove("popup_opened");
 }
 
 function save(event) {
@@ -25,11 +25,11 @@ function save(event) {
   closePopup();
 }
 
-function like(event) {
-  event.target.classList.toggle("element__like-button_active");
-}
+// function like(event) {
+//   event.target.classList.toggle("element__like-button_active");
+// }
 
 popupForm.addEventListener("submit", save);
 popupOpenButton.addEventListener("click", openPopup);
 popupCloseButton.addEventListener("click", closePopup);
-likeButtons.forEach((button) => button.addEventListener("click", like));
+// likeButtons.forEach((button) => button.addEventListener("click", like));
