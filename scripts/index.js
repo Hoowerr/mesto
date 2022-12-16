4dd6342fb48434a297622cf618eaee33b61efa4b
Hoowerr <109082395+Hoowerr@users.noreply.github.com>
2022-12-16 22:18:52 +0300

    feat add new popup

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,21 @@
-const popup = document.querySelector(".popup");
-const popupOpenButton = document.querySelector(".profile__edit-button");
-const popupCloseButton = document.querySelector(".popup__closed");
-const profileTitle = document.querySelector(".profile__title");
-const profileSubtitle = document.querySelector(".profile__subtitle");
-const popupNameInput = document.getElementById("NameInputId");
-const popupJobInput = document.getElementById("JobInputId");
-const popupForm = document.querySelector(".popup__form");
-// const likeButtons = document.querySelectorAll(".element__like-button");
+const popupEdit = document.querySelector('.popup_prof');
+const popupOpenButton = document.querySelector('.profile__edit-button');
+const popupCloseButton = document.querySelector('.popup__closed');
+const profileTitle = document.querySelector('.profile__title');
+const profileSubtitle = document.querySelector('.profile__subtitle');
+const popupNameInput = document.getElementById('NameInputId');
+const popupJobInput = document.getElementById('JobInputId');
+const popupForm = document.querySelector('.popup__form');
+const likeButtons = document.querySelectorAll('.element__like-button');
 
 function openPopup() {
-  popup.classList.add("popup_opened");
+  popupEdit.classList.add('popup_opened');
   popupNameInput.value = profileTitle.textContent;
   popupJobInput.value = profileSubtitle.textContent;
 }
 
 function closePopup() {
-  popup.classList.remove("popup_opened");
+  popupEdit.classList.remove('popup_opened');
 }
 
 function save(event) {
@@ -25,11 +25,29 @@ function save(event) {
   closePopup();
 }
 
-// function like(event) {
-//   event.target.classList.toggle("element__like-button_active");
-// }
+function like(event) {
+  event.target.classList.toggle('element__like-button_active');
+}
 
-popupForm.addEventListener("submit", save);
-popupOpenButton.addEventListener("click", openPopup);
-popupCloseButton.addEventListener("click", closePopup);
-// likeButtons.forEach((button) => button.addEventListener("click", like));
+popupForm.addEventListener('submit', save);
+popupOpenButton.addEventListener('click', openPopup);
+popupCloseButton.addEventListener('click', closePopup);
+likeButtons.forEach((button) => button.addEventListener('click', like));
+
+// попап добавления фотографий
+
+const popupAdd = document.querySelector('.popup_add');
+const popupAddButton = document.querySelector('.profile__add-button');
+const popupAddCloseButton = document.querySelector('.popup__add-closed');
+
+
+function openPopupAdd () {
+  popupAdd.classList.add('popup_opened');
+}
+
+function closePopupAdd () {
+  popupAdd.classList.remove('popup_opened');
+}
+
+popupAddButton.addEventListener('click', openPopupAdd);
+popupAddCloseButton.addEventListener('click', closePopupAdd);
